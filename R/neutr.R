d819cd38d4ae1db2a752f9acb018eebf7b945aea
jeromechave <jerome.chave@univ-tlse3.fr>
2024-05-06 12:59:44 +0200

    Minor changes
    vignettes changed (proper math equations) and optim.multideme checked

--- R/neutr.R
+++ R/neutr.R
@@ -15,18 +15,18 @@ usethis::use_package("nloptr")
 zeroremove=function(x){ y=x[x!=0]; z=y[!is.na(y)]; z}
 
 
-#' Maximum likelihood estimation of theta based on Ewens sampling formula
+#' Maximum likelihood estimation of \eqn{\theta} based on Ewens sampling formula
 #'
-#' @description optim.ewens computes the maximum likelihood estimate of Ewens theta parameter
-#' for a given species abundance distribution {n_1, ..., n_k}
-#' where n_i is the number of organisms of species i in the sample
-#' and k is the total number of species (for all i, n_i > 0)
+#' @description optim.ewens computes the maximum likelihood estimate of Ewens \eqn{\theta} parameter
+#' for a given species abundance distribution \eqn{(n_1, ..., n_k)}
+#' where \eqn{n_i} is the number of organisms of species i in the sample
+#' and k is the total number of species in the sample (for all \eqn{i, n_i > 0}).
 #' The input should be a vector
 #'
 #' @param input_abundances a vector of integers
 #'
-#' @return theta parameter of Ewens sampling formula, a real value
-#' @return logl maximal value of the log-likelihood, a real value
+#' @return theta the \eqn{\theta} parameter of Ewens sampling formula, a real value
+#' @return logl the maximal value of the log-likelihood, a real value
 #' @export
 #'
 #' @examples
@@ -55,33 +55,33 @@ optim.ewens = function(input_abundances){
     eval_f = logL,
     lb=c(0.0),
     ub=c(as.numeric(J)),
-    opts = list("algorithm" = "NLOPT_LN_SBPLX", maxeval=2000)
+    opts = list("algorithm" = "NLOPT_LN_SBPLX")
   )
   # return MLE of theta, and value of the logL function at MLE_theta
   return(list(theta=optp$solution[1],logl=--optp$objective))
 
 }
 
-#' Maximum likelihood estimation of local immigration rates m_j based on multi-deme sampling formula
+#' Maximum likelihood estimation of local immigration rates \eqn{(m_1,...,m_K)} based on the K-deme sampling formula
 #'
 #' @description optim.multideme computes the maximum likelihood estimate for the multi-deme parameters
-#' for a given species abundance matrix {n_j1, ..., n_jk} for deme j
-#' where n_ji is the number of organisms of species i in deme j
-#' and k is the total number of species (for all i, n_i >= 0).
+#' for a given species abundance matrix \eqn{(n_{1j}, ..., n_{kj})} for deme j (\eqn{j\in\{1,...,K\}})
+#' where \eqn{n_{ij}} is the number of organisms of species i in deme j
+#' and k is the total number of species (for all \eqn{i, n_i \geq 0}).
 #' The formulation in this description is that of species abundance distributions
-#' but is valid for any partitions in a subdivided setting (word frequencies in multiple books ..)
+#' but is valid for any partition in a subdivided setting (e.g., word frequencies in multiple books).
 #'
-#' @param input_abundance_matrix a matrix (n_ij) of abundances for species i in deme (local site) j
+#' @param input_abundance_matrix a matrix \eqn{n_{ij}} of abundances for species i in deme (local site) j
 #'
-#' @return I rescaled immigration rate, I=m/(1-m)*(J-1), a vector of real numbers
+#' @return I rescaled immigration rate, \eqn{I=\frac{m}{1-m}(J-1)}, a vector of real numbers
 #' @return m immigration rate, a vector of real numbers
 #' @return J local community size, a vector of integers
 #' @return k number of classes/species, a vector of integers
 #' @export
 #'
 #' @examples
-#' input_abundances1=c(234,87,34,5,4,3,3,2,2,1,1,1,1)
-#' input_abundances2=c(240,20,48,2,21,1,3,2,5,2,1,1,1)
+#' input_abundances1=c(44,37,34,5,4,3,3,2,2,1,1,1,1)
+#' input_abundances2=c(240,20,48,2,21,1,3,2,5,2,0,1,1)
 #' input_abundance_matrix=rbind(input_abundances1,input_abundances2)
 #' optim.multideme(input_abundance_matrix)
 optim.multideme = function(input_abundance_matrix){
@@ -95,13 +95,15 @@ optim.multideme = function(input_abundance_matrix){
   J=rep(0,demes)
   k=rep(0,demes)
   for(j in 1:demes){
-    local_abundance=input_abundance_matrix[,j]
+    local_abundance=as.numeric(input_abundance_matrix[j,])
     k[j]=length(zeroremove(local_abundance))
-    J[j]=sum(zeroremove(local_abundance))
-    # -loglikelihood function
+    J0=sum(zeroremove(local_abundance));J[j]=J0
+    list1=which(local_abundance != 0)
+    x1=as.numeric(x[list1]);local_abundance1=local_abundance[list1]
 
+    # -loglikelihood function
     logL = function(I){
-      value=sum(lgamma(I*x+local_abundance)-lgamma(I*x))-lgamma(I+J[j])+lgamma(I)
+      value=sum(lgamma(I*x1+local_abundance1)-lgamma(I*x1))-lgamma(I+J0)+lgamma(I)
       -value
     }
     if(k[j]>1 && J[j]>1){
@@ -114,6 +116,7 @@ optim.multideme = function(input_abundance_matrix){
       )
       I[j]=optp$solution[1]
     }
+    print(paste("Deme: ", j, "..."))
 
   }
   m=I/(J-1+I)
@@ -125,22 +128,22 @@ optim.multideme = function(input_abundance_matrix){
 
 }
 
-#' Maximum likelihood estimation of theta and sigma based on Pitman sampling formula
+#' Maximum likelihood estimation of \eqn{\theta} and \eqn{\sigma} based on Pitman sampling formula
 #'
-#' @description optim.pitman computes the maximum likelihood estimate of Pitman (theta,sigma) parameters
-#' for a given species abundance distribution {n_1, ..., n_k}
-#' where n_i is the number of organisms of species i in the sample
-#' and k is the total number of species (for all i, n_i > 0)
-#' The input should be a vector and initial values of (theta,sigma)
-#' The argument should be init_vals=c(theta_init,sigma_init)
-#' with the condition theta_init>0.0 and 1.0 > sigma_init > 0.0
+#' @description optim.pitman computes the maximum likelihood estimate of Pitman \eqn{(\theta,\sigma)} parameters
+#' for a given species abundance distribution \eqn{(n_1, ..., n_k)}
+#' where \eqn{n_i} is the number of organisms of species i in the sample
+#' and k is the total number of species (for all \eqn{i, n_i > 0})
+#' The input should be a vector and initial values of \eqn{(\theta,\sigma)}
+#' with the argument init_vals=c(theta_init,sigma_init)
+#' with the condition theta_init>0.0, and 1.0 > sigma_init > 0.0.
 #'
 #' @param input_abundances a vector of integers
 #' @param init_vals initial values of (theta,sigma), a vector of two real numbers
 #'
-#' @return theta a real value
-#' @return sigma a real value
-#' @return log-likelihood a real value
+#' @return theta parameter \eqn{\theta}, a real value
+#' @return sigma parameter \eqn{\sigma}, a real value
+#' @return logl, maximal log-likelihood, a real value
 #' @export
 #'
 #' @examples
@@ -176,18 +179,17 @@ optim.pitman = function(input_abundances,init_vals=c(10.0,0.1)){
 
 ###### GENERATING URN MODELS   ######################
 
-#' Generation of a neutral partition given parameter theta using Hoppe urn model
+#' Generation of a neutral partition given parameter \eqn{\theta} using Hoppe urn model
 #'
-#' @description generate.hoppe.urn0 creates a rank-abundance distribution based on Hoppe's urn scheme for a given theta
-#' and sampling size nb_balls = J
-#' Starting at zero species, corresponding to the single black ball with weight theta.
-#' If picked, a new class is created, else the picked ball is duplicated
-#' (abundance increases by one unity)
-#' Added a much faster random number generator
-#' https://cran.r-project.org/web/packages/dqrng/vignettes/dqrng.html
+#' @description generate.hoppe.urn0 creates a rank-abundance distribution based on Hoppe's urn scheme
+#' for a given parameter \eqn{\theta} and sampling size J.
+#' The process starts at zero species, corresponding to the single black ball with weight \eqn{\theta}.
+#' If the black ball is picked, a new class is created, else the picked colored ball is duplicated
+#' (its abundance increases by one unity).
+#' Added a much faster random number generator (dqrng package).
 #'
-#' @param theta is a real value
-#' @param J is the number of individuals, an integer
+#' @param theta parameter \eqn{\theta}, a real value
+#' @param J  number of individuals, an integer
 #'
 #' @return output_abundances is a vector of integers
 #' @return k is the expected number of classes, a real number
@@ -232,14 +234,14 @@ generate.hoppe.urn0 = function(theta,J) {
   return(output_abundances)
 }
 
-#' Generation of a neutral partition given parameter theta using the Griffith-Engen-McCloskey broken stick model
+#' Generation of a neutral partition given parameter \eqn{\theta} using the Griffith-Engen-McCloskey representation
 #'
-#' @description generate.hoppe.urn creates a rank-abundance distribution for a given theta and J objects.
-#' Beta-distributed random variables are picked from distribution Beta(1,theta),
-#' and the partition is the multinomial draw of J individuals drawn according to the GEM random variables
+#' @description generate.hoppe.urn creates a rank-abundance distribution for a given \eqn{\theta} and J.
+#' Beta-distributed random variables are picked from distribution \eqn{Beta(1,\theta)},
+#' and the ressulting partition is one multinomial draw of J individuals drawn according to the GEM random variables.
 #'
-#' @param theta is a real value
-#' @param J is the number of individuals, an integer
+#' @param theta parameter \eqn{\theta}, a real value
+#' @param J  number of individuals, an integer
 #'
 #' @return output_abundances is a vector of integers
 #' @return k is the expected number of classes, a real number
@@ -284,22 +286,22 @@ generate.hoppe.urn = function(theta,J){
 }
 
 
-#' Generation of a Pitman partition given parameters theta and sigma using the Griffith-Engen-McCloskey broken stick model
+#' Generation of a Pitman partition given parameters \eqn{\theta} and \eqn{\sigma} from the Griffith-Engen-McCloskey representation
 #'
-#' @description The Pitman urn scheme is as follows: starting at zero species, corresponding to the single black ball with weight theta.
+#' @description The Pitman urn scheme is as follows: starting at zero species, corresponding to the single black ball with weight \eqn{\theta}
 #' a species i, of k species total, is selected with probability:
-#' {(n_i-sigma)/(n+theta)},
-#' sigma is non-null, n is the current number of balls
-#' the probability to pick the black ball i {(k*sigma+theta)/(n+theta)}.
+#' \eqn{(n_i-\sigma)/(n+\theta)},
+#' \eqn{\sigma} is strictly between 0.0 and 1.0, n is the current number of balls
+#' the probability to pick the black ball i \eqn{(k\sigma+\theta)/(n+\theta)}.
 #' This function generate.pitman.urn creates a rank-abundance distribution for a given theta,sigma and J objects.
-#' Beta-distributed random variables are picked from distribution Beta(1-sigma,theta+k*sigma),
+#' Beta-distributed random variables are picked from distribution \eqn{Beta(1-\sigma,\theta+k\sigma)},
 #' and the partition is the multinomial draw of J individuals drawn according to the GEM random variables
 #'
-#' @param theta is a real value
-#' @param sigma is a real value
+#' @param theta parameter \eqn{\theta},  a real value
+#' @param sigma parameter \eqn{\sigma},  a real value
 #' @param J is the number of individuals, an integer
 #'
-#' @return output_abundances a vector of integers
+#' @return output_abundances is a vector of integers
 #' @return k is the expected number of classes, a real number
 #' @export
 #'
